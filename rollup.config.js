7defd50beb98ff17e71e88afa8eb0300ad717f9f
ramashish07 <iec2020095@iiita.ac.in>
2024-01-29 04:27:24 +0530

    color interaction added

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+const buble = require('@rollup/plugin-buble');
+  export default {
+    input: 'index.js',
+    external: ['d3', 'react', 'react-dom'],
+    output: {
+      file: 'bundle.js',
+      format: 'iife',
+      sourcemap: true,
+      globals: {
+        d3: 'd3',
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+    },
+    plugins: [buble()],
+  };
